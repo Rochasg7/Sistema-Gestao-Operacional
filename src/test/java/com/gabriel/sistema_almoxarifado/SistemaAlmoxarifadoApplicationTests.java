8ba1127b4c03e2b9d3648b5c8fd5c1f683d8cc28
Gabriel <gabrielrocha.dev2025@gmail.com>
2026-09-15 21:16:07 -0300

    SG: Model

---
+++ src/test/java/com/gabriel/sistema_almoxarifado/SistemaAlmoxarifadoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.gabriel.sistema_almoxarifado;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SistemaAlmoxarifadoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
